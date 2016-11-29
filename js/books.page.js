86f2a8889fdda242103cdff7941cde81052bd542
EmilEhrenreich <emil.ehrenreich@gmail.com>
2016-11-29 11:10:58 +0100

    TIlføjet oprete bruger UI

--- js/books.page.js
+++ js/books.page.js
@@ -1,25 +1,47 @@
+var urlBook = "http://localhost:8080/DIS_Server_Privat_war_exploded/book";
+
+
+
 $(document).ready(function () {
 
     var $booksTableBody = $("#booksTableBody");
 
     $.ajax({
-        url: "http://localhost:8080/server2_0_war_exploded/book",
+
+        url: "http://localhost:8080/DIS_Server_Privat_war_exploded/book",
         method: "GET",
 
 
         dataType: "json",
         contentType: "application/json",
-        success: function (books, status, xhr) {
+        success: function (data, status, xhr) {
 
-            books.forEach(function (book) {
+            var dec = encryptDecrypt(data);
+            var tests = JSON.parse(dec);
+
+
+
+            tests.forEach(function (test) {
 
                 $booksTableBody.append(
                     "<tr>" +
-                    "<td>" + books.Title + "</td>" +
+                    "<td>" + test.bookID    + "</td>" +
+                    "<td>" + test.title     + "</td>" +
+                    "<td>" + test.version   + "</td>" +
+                    "<td>" + test.ISBN      + "</td>" +
+                    "<td>" + test.priceAB   + "</td>" +
+                    "<td>" + test.priceSAXO + "</td>" +
+                    "<td>" + test.priceCDON + "</td>" +
+                    "<td>" + test.publisher + "</td>" +
+                    "<td>" + test.author    + "</td>" +
+
+
+
                     "</tr>"
                 );
 
             });
+
 
         },
         error: function(a, b, c){
@@ -28,3 +50,16 @@ $(document).ready(function () {
 
     });
 });
+
+function encryptDecrypt(input) {
+    var key = ['A', 'B', 'C']; //Can be any chars, and any size array
+    var output = [];
+
+    for (var i = 0; i < input.length; i++) {
+        var charCode = input.charCodeAt(i) ^ key[i % key.length].charCodeAt(0);
+        output.push(String.fromCharCode(charCode));
+    }
+    return output.join("");
+}
+
+
